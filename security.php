b18203d6400cb42af587833bce0b20c1c986eebe
kulasusu <38985525+kulasusu@users.noreply.github.com>
2020-08-22 15:12:38 +0700

    Update security.php

--- security.php
+++ security.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="/action_page.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+<p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
 <?php
 function decode($string){
 	$decoded = "";
@@ -19,3 +34,9 @@ $user = $bol[0];
 $pass = $bol[1];
 echo md5($user.$pass.$random);
 ?>
+</body>
+</html>
+
+
+
+
